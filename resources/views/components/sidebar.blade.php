53aaaab74ae3f79d455ba9f61bb85b613764987d
Abd. Asis <id.abdasis@gmail.com>
2021-07-07 10:05:40 +0700

    update data sidebar

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -80,6 +80,13 @@
                         </a>
                     </li><!-- .nk-menu-item -->
 
+                    <li class="nk-menu-item">
+                        <a href="{{route('absensi.semua')}}" class="nk-menu-link">
+                            <span class="nk-menu-icon"><em class="icon ni ni-navigate"></em></span>
+                            <span class="nk-menu-text">Absensi</span>
+                        </a>
+                    </li><!-- .nk-menu-item -->
+
 
 
                 </ul><!-- .nk-menu -->
